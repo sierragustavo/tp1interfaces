0146d85e6ee73a3b9c8b8d3e609c1b61bbb376ca
Kevin Garces Hoyberg <kevin.garceshoyberg@gmail.com>
2022-09-08 23:23:50 -0300

    fixed moveTo() Circulo

--- eventos.js
+++ eventos.js
@@ -2,14 +2,14 @@ let canvasEventos = document.getElementById("canvas");
 let elemLeft = canvasEventos.offsetLeft + canvasEventos.clientLeft;
 let elemTop = canvasEventos.offsetTop + canvasEventos.clientTop;
 let seleccionado = undefined;
-/*
+
 canvasEventos.addEventListener("click", function (event) {
   var x = event.pageX - elemLeft,
     y = event.pageY - elemTop;
   figuras.forEach(function (element) {
     element.seleccionar(x, y);
   });
-});*/
+});
 
 canvasEventos.addEventListener("mousedown", function (event) {
   var x = event.pageX - elemLeft,
@@ -17,8 +17,6 @@ canvasEventos.addEventListener("mousedown", function (event) {
   figuras.forEach(function (element) {
     if (element.estaSeleccionado(x, y)) {
       seleccionado = element;
-    } else {
-      seleccionado = undefined;
     }
   });
 });
@@ -30,11 +28,12 @@ canvasEventos.addEventListener("mouseup", function (event) {
 });
 
 document.body.addEventListener("keydown", onKeyDown);
+
 function onKeyDown(event) {
   if (seleccionado != undefined) {
     var x = seleccionado.posX,
       y = seleccionado.posY;
-    if (event.key == "ArrowUp") seleccionado.moveTo(x, y -10);
+    if (event.key == "ArrowUp") seleccionado.moveTo(x, y - 10);
     if (event.key == "ArrowDown") seleccionado.moveTo(x, y + 10);
     if (event.key == "ArrowLeft") seleccionado.moveTo(x - 10, y);
     if (event.key == "ArrowRight") seleccionado.moveTo(x + 10, y);
--- figuras/circulo.js
+++ figuras/circulo.js
@@ -6,15 +6,7 @@ class Circulo extends Figura {
   draw() {
     ctx.fillStyle = this.fill;
     ctx.beginPath();
-    ctx.arc(
-      this.posX,
-      this.posY,
-      this.width / 2,
-      0,
-      2 * Math.PI,
-      0,
-      2 * Math.PI
-    );
+    ctx.arc(this.posX, this.posY, this.width / 2, 0, 2 * Math.PI, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
   }
@@ -24,5 +16,14 @@ class Circulo extends Figura {
     let _y = this.posY - y;
     return Math.sqrt(_x * _x + _y * _y) < this.width;
   }
-  moveTo(posX, posY) {}
+  moveTo(x, y) {
+    ctx.clearRect(this.posX - this.width / 2 - 1, this.posY - this.width / 2 - 1, this.width / 2 * 2 + 2, this.width / 2 * 2 + 2);
+    ctx.fillStyle = this.fill;
+    ctx.beginPath();
+    ctx.arc(x, y, this.width / 2, 0, 2 * Math.PI, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    this.posX = x;
+    this.posY = y;
+  }
 }
